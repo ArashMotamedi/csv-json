5777f2c59a6fb3032e9ecb31003c4326db4ea2c5
Arash Motamedi <arashm@microsoft.com>
2019-08-19 22:49:12 -0700

    Base page

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
+const styles = require("./app.module.css");
 
 export function App() {
-  return <h1>Hello</h1>
+  const [source, setSource] = useState("");
+  const [result, setResult] = useState("");
+
+  function updateSource(value: string) {
+    setSource(value);
+    setResult(value);
+  }
+
+  return <div className={styles.body}>
+    <h1>JSON-CSV Utility</h1>
+    <div className={styles.container}>
+      <div className={styles.left}><textarea value={source} onChange={e => updateSource(e.target.value)} /></div>
+      <div className={styles.right}><textarea readOnly value={result} /></div>
+    </div>
+  </div>
 }
